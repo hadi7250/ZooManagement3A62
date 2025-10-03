13a69702781d88876f1608ba548d0eca45491745
Hadi <Hassan.Hadi@esprit.tn>
2025-10-03 15:30:18 +0100

    init exercice

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -2,14 +2,18 @@ import java.util.Scanner;
 
 class ZooManagement {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
             int nbrCages;
             String zooName;
+
+        Scanner input = new Scanner(System.in);
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter the name of the Zoo: ");
         zooName = sc.nextLine();
+
         System.out.println("Entre the number of the cages: ");
         nbrCages = sc.nextInt();
+
         System.out.println("Zoo name: " + zooName);
         System.out.println("Number of the cages: " + nbrCages);
     }
