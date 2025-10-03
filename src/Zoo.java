01b0aa6bf9bf33c09d245a1fad43f54305454f9c
Hadi <Hassan.Hadi@esprit.tn>
2025-10-03 17:29:43 +0100

    commit 2

--- src/Zoo.java
+++ src/Zoo.java
@@ -1,19 +1,30 @@
-class Zoo {
-    public static void displayZoo(Animal[] animals) {
-        String name;
-        String city;
-        int nbrCages;
+public class Zoo {
+        private Animal[] animals;
+        private String name;
+        private String city;
+        private int nbrCages;
 
         public Zoo(String name, String city, int nbrCages){
+            this.animals = new Animal[25];
             this.name = name;
             this.city = city;
             this.nbrCages = nbrCages;
         }
 
-        Animal a = new Animal(Lion, Simba, 8, true);
-        Zoo z = new Zoo(Belvedere, Tunis, 30);
+            void displayZoo() {
+                System.out.println("my Zoo " + name + " in " + city + " with " + nbrCages + " cages.");
+            }
 
-        System.out.println(MyZoo);
-        System.out.println(MyZoo.toString());
+    public static void main(String[] args) {
+        Animal a1 = new Animal("Lion", "Simba", 8, true);
+        Animal a2 = new Animal("Singe", "Darwin", 10, true);
+        Animal a3 = new Animal("Ursidae", "Baloo", 12, true);
+
+        Zoo myZoo = new Zoo("Belvedere", "Tunis", 15);
+
+        myZoo.displayZoo();
     }
+
 }
+
+
