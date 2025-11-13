173e7217910abf7819ab22088229a1f78a3e0bc6
Hadi <Hassan.Hadi@esprit.tn>
2025-11-13 20:30:07 +0100

    finished

--- src/Employe.java
+++ src/Employe.java
@@ -1,2 +1,50 @@
-public class Employe {
+public class Employe implements Comparable <Employe>{
+    private int id, grade;
+    private String nom, prenom, departement;
+
+    public Employe(int id, String nom, String prenom, String departement, int grade) {
+        this.id = id;
+        this.grade = grade;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.departement = departement;
+    }
+
+    public int getGrade() {return grade;}
+    public void setGrade(int grade) {this.grade = grade;}
+
+    public int getId() {return id;}
+    public void setId(int id) {this.id = id;}
+
+    public String getNom() {return nom;}
+    public void setNom(String nom) {this.nom = nom;}
+
+    public String getPrenom() {return prenom;}
+    public void setPrenom(String prenom) {this.prenom = prenom;}
+
+    public String getDepartement() {return departement;}
+    public void setDepartement(String departement) {this.departement = departement;}
+
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (!(o instanceof Employe other)) return false;
+        return this.id == other.id && this.nom.equalsIgnoreCase(other.nom);
+    }
+
+    @Override
+    public String toString() {
+        return "Employe{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", departement='" + departement + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Employe other) {
+        return Integer.compare(this.id, other.id);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,32 @@
 public class Main {
+    public static void main(String[] args) {
+        SocieteArrayList societe = new SocieteArrayList();
+
+        Employe e1 = new Employe(51235, "Ahmed", "Mohamed", "HR", 3);
+        Employe e2 = new Employe(23156, "Abdullah", "Sami", "Finance", 2);
+        Employe e3 = new Employe(32114, "Hedi", "Ammar", "Maintenance", 4);
+
+        societe.ajouterEmploye(e1);
+        societe.ajouterEmploye(e2);
+        societe.ajouterEmploye(e3);
+
+        System.out.println("List of employees: ");
+        societe.displayEmploye();
+
+        System.out.println("\n search by name (Hedi): " + societe.rechercherEmploye("Hedi"));
+        System.out.println("\n search by name (khouna): " + societe.rechercherEmploye("khouna"));
+
+
+        System.out.println("\n Suppression of e2:");
+        societe.supprimerEmploye(e2);
+        societe.displayEmploye();
+
+        System.out.println("\n sorting by id: ");
+        societe.trierEmployeParId();
+        societe.displayEmploye();
+
+        System.out.println("\n sorting by name, department and grade: ");
+        societe.trierEmployeParNomDepartementEtGrade();
+        societe.displayEmploye();
+    }
 }
